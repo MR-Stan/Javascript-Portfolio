b1ec88042fbc8a7510e943cf1a114350b2d43e42
MR-Stan <michaelrstanley87@gmail.com>
2019-11-11 14:20:14 -0600

    style updates, reformatted contact section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
 // when the page is loaded, hide everything but the landing section 
 $(function () {
-    $(".content").hide();
+    $(".hide").hide();
 }) 
 
 // when a button is clicked, landing page is hidden, content is shown, and user is taken to corresponding section
 $(".showContent").click(function() {
     $("#landing").hide();
-    $(".content").show();
+    $(".hide").show();
     $("body").css("background", "black");
 })
 
