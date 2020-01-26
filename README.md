f89f4b4ba9e0c92d4c85f1f9683ee1451a1a484c
MR-Stan <michaelrstanley87@gmail.com>
2020-01-26 14:43:59 -0600

    removed footer, updated links to jquery functions that hide all content and show corresponding section

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Mike-Stanley-Portfolio
-My portfolio was built with HTML, CSS, Bootstrap, and Javascript.
+My portfolio was built with HTML, CSS, Bootstrap, Javascript, and jQuery.
 
 ## Check it out: https://mr-stan.github.io/Mike-Stanley-Portfolio/
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,37 @@
-$('#aboutLink').click(function (e) {
-    e.preventDefault();
-    alert('success');
+$(document).ready(function () {
+    $('.content').hide();
+    $('#aboutSection').show();
+    
+    // when about link is clicked, hide all but about section
+    $('#aboutLink').click(function (e) {
+        e.preventDefault();
+        $('.content').hide();
+        $('#aboutSection').show();
+    });
+
+    // when skills link is clicked, hide all but skills section
+    $('#skillsLink').click(function (e) {
+        e.preventDefault();
+        $('.content').hide();
+        $('#skillsSection').show();
+    });
+
+    $('#resumeLink').click(function (e) {
+        e.preventDefault();
+        $('.content').hide();
+        $('#resumeSection').show();
+    });
+
+    $('#projectsLink').click(function (e) {
+        e.preventDefault();
+        $('.content').hide();
+        $('#projectsSection').show();
+    });
+
+    $('#contactLink').click(function (e) {
+        e.preventDefault();
+        $('.content').hide();
+        $('#contactSection').show();
+    });
+
 });
